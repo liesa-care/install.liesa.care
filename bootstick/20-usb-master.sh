146bcc15428052f2f88c093eaa881c5b1cee8fb0
dezi <dezi@kappa-mm.de>
2023-05-15 21:01:34 +0200

    Fixed fucking boot usb stick partitions.

--- bootstick/20-usb-master.sh
+++ bootstick/20-usb-master.sh
@@ -25,6 +25,15 @@ sudo apt -y install openssh-server
 sudo apt -y update
 sudo apt -y upgrade
 
+echo "Enable fucking USB 2.0 mode on Minis Forum HM50 etc."
+MMU_PRESENT=$(grep amd_iommu=on /etc/default/grub)
+if [ -n "$MMU_PRESENT" ]; then
+  echo "Already done..."
+else
+  sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="amd_iommu=on iommu.passthrough=on"/g' /etc/default/grub
+  sudo update-grub
+fi
+
 echo "Liesa Care APT Repository"
 APT_PRESENT=$(grep apt.liesa.care /etc/apt/sources.list)
 if [ -n "$APT_PRESENT" ]; then
@@ -38,3 +47,24 @@ fi
 sudo apt -y update
 sudo apt -y autoremove
 sudo apt -y clean
+
+#
+# Check /etc/fstab
+#
+# Fucking Ubuntu installer tends to mount
+# the build in disk boot partition for this
+# USB stick. So get the real partition id
+# and correct it manually.
+#
+# Correct mounts:
+#
+# /dev/sdb2       58310644 8276088  47040076  15% /
+# /dev/sdb1         523248       4    523244   1% /boot/efi
+#
+# Fucked up mounts:
+#
+# /dev/sdb2       58310644 8276088  47040076  15% /
+# /dev/sda1         523248       4    523244   1% /boot/efi
+#
+# In this case sudo vi /etc/fstab and fix the uuids.
+#
