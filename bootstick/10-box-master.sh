801f4df571df043d6bf12f0f8d650970d0ec96f3
dezi <dezi@kappa-mm.de>
2023-05-15 20:53:38 +0200

    Fixed fucking rear USB bug on minis forum HM50.

--- bootstick/10-box-master.sh
+++ bootstick/10-box-master.sh
@@ -25,6 +25,15 @@ sudo apt -y install openssh-server
 sudo apt -y update
 sudo apt -y upgrade
 
+echo "Enable fucking USB 2.0 mode on Minis Forum HM50 etc."
+MMU_PRESENT=$(grep amd_iommu=on /etc/default/grub)
+if [ -n "$MMU_PRESENT" ]; then
+  echo "Already done..."
+else
+  sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="amd_iommu=on iommu.passthrough=on"/g' /etc/default/grub
+  sudo update-grub
+fi
+
 echo "Liesa Care APT Repository"
 APT_PRESENT=$(grep apt.liesa.care /etc/apt/sources.list)
 if [ -n "$APT_PRESENT" ]; then
@@ -40,3 +49,6 @@ sudo apt -y autoremove
 sudo apt -y clean
 
 sudo apt -y install box.webdog.debug
+
+sudo reboot
+
