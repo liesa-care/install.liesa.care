e2d7420ae6bf6c853ab3889e716c3ac5f5b3c025
dezi <dezi@kappa-mm.de>
2023-08-13 06:47:56 +0200

    Bluez patch.

--- bluetooth/01-rebuild.sh
+++ bluetooth/01-rebuild.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+export ARCH=arm64
+export GLIB2=libglib2.0-0_2.77.1-2
+export BLUEZ=bluez_5.68-0ubuntu1
+
+#
+# Unpack deb files.
+#
+
+
+
+
 
 ./configure --prefix=/usr --mandir=/usr/share/man \
 	--sysconfdir=/etc --localstatedir=/var \
