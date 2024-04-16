e2c2ba93c14e11b6f9d2dd07e735728bf0769895
dezi <dezi@kappa-mm.de>
2024-04-16 12:24:26 +0200

    Vretti TP6 printer debug.

--- apksigner/tp6.sh
+++ apksigner/tp6.sh
@@ -1,6 +1,25 @@
 
 cd ~/tv-apps/tp6
 
+mv ~/go/src/github.com/dezi/project.java.tvbox/app/build/outputs/apk/debug/app-debug.apk ./de.kappa.tvbox.apk
+
+rm -rf de.kappa.tvbox
+
+apktool if de.kappa.tvbox.apk
+apktool d de.kappa.tvbox.apk
+
+rm -rf ./patch
+cp -rp ./de.kappa.tvbox/smali_classes22/patch .
+
+
+
+
+
+
+
+
+cd ~/tv-apps/tp6
+
 rm -rf com.yoyinprintera4.app
 
 apktool if com.yoyinprintera4.app.apk
@@ -9,7 +28,11 @@ apktool d com.yoyinprintera4.app.apk
 
 
 
+cp -rp ./patch ./com.yoyinprintera4.app/smali
 
+
+vi ./com.yoyinprintera4.app/smali_classes3/com/printer/PrinterManager\$PrintRunnable.smali
+vi ./com.yoyinprintera4.app/smali_classes3/com/printer/PrinterManager\$ConnectRunnable.smali
 
 #####
 
@@ -26,5 +49,4 @@ cp com.yoyinprintera4.app.aligned.apk com.yoyinprintera4.app.resigned.apk
 blabla
 EOF
 
-adb connect 8B3X136F0
-adb -s 8B3X136F0 install -r com.yoyinprintera4.app.resigned.apk
+adb -s 8B3X136F0 install com.yoyinprintera4.app.resigned.apk
