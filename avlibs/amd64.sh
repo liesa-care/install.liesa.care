118b63da84418b7841996cf6af50ad4ec3ef49d9
dezi <dezi@kappa-mm.de>
2024-10-01 12:31:51 +0200

    FFMpeg avlibs.

--- avlibs/amd64.sh
+++ avlibs/amd64.sh
@@ -4,12 +4,12 @@
 # Collect ffmpeg libs for amd64.
 #
 
-scp /usr/lib/x86_64-linux-gnu/libavcodec.so.58 dennis-mb:/opt/box/gen/avlibs/x86_64-linux-gnu
-scp /usr/lib/x86_64-linux-gnu/libavutil.so.56 dennis-mb:/opt/box/gen/avlibs/x86_64-linux-gnu
-scp /usr/lib/x86_64-linux-gnu/libswscale.so.5 dennis-mb:/opt/box/gen/avlibs/x86_64-linux-gnu
-scp /usr/lib/x86_64-linux-gnu/libswresample.so.3 dennis-mb:/opt/box/gen/avlibs/x86_64-linux-gnu
-scp /usr/lib/x86_64-linux-gnu/libvpx.so.7 dennis-mb:/opt/box/gen/avlibs/x86_64-linux-gnu
-scp /usr/lib/x86_64-linux-gnu/libdav1d.so.5 dennis-mb:/opt/box/gen/avlibs/x86_64-linux-gnu
-scp /usr/lib/x86_64-linux-gnu/libcodec2.so.1.0 dennis-mb:/opt/box/gen/avlibs/x86_64-linux-gnu
-scp /usr/lib/x86_64-linux-gnu/libx264.so.163 dennis-mb:/opt/box/gen/avlibs/x86_64-linux-gnu
-scp /usr/lib/x86_64-linux-gnu/libmfx.so.1 dennis-mb:/opt/box/gen/avlibs/x86_64-linux-gnu
+scp /usr/lib/x86_64-linux-gnu/libavcodec.so.58 dennis-mb:/opt/box/gen/avlibs/amd64
+scp /usr/lib/x86_64-linux-gnu/libavutil.so.56 dennis-mb:/opt/box/gen/avlibs/amd64
+scp /usr/lib/x86_64-linux-gnu/libswscale.so.5 dennis-mb:/opt/box/gen/avlibs/amd64
+scp /usr/lib/x86_64-linux-gnu/libswresample.so.3 dennis-mb:/opt/box/gen/avlibs/amd64
+scp /usr/lib/x86_64-linux-gnu/libvpx.so.7 dennis-mb:/opt/box/gen/avlibs/amd64
+scp /usr/lib/x86_64-linux-gnu/libdav1d.so.5 dennis-mb:/opt/box/gen/avlibs/amd64
+scp /usr/lib/x86_64-linux-gnu/libcodec2.so.1.0 dennis-mb:/opt/box/gen/avlibs/amd64
+scp /usr/lib/x86_64-linux-gnu/libx264.so.163 dennis-mb:/opt/box/gen/avlibs/amd64
+scp /usr/lib/x86_64-linux-gnu/libmfx.so.1 dennis-mb:/opt/box/gen/avlibs/amd64
--- avlibs/arm64.sh
+++ avlibs/arm64.sh
@@ -4,12 +4,12 @@
 # Collect ffmpeg libs for arm64.
 #
 
-scp /usr/lib/aarch64-linux-gnu/libavcodec.so.58 dennis-mb:/opt/box/gen/avlibs/aarch64-linux-gnu
-scp /usr/lib/aarch64-linux-gnu/libavutil.so.56 dennis-mb:/opt/box/gen/avlibs/aarch64-linux-gnu
-scp /usr/lib/aarch64-linux-gnu/libswscale.so.5 dennis-mb:/opt/box/gen/avlibs/aarch64-linux-gnu
-scp /usr/lib/aarch64-linux-gnu/libswresample.so.3 dennis-mb:/opt/box/gen/avlibs/aarch64-linux-gnu
-scp /usr/lib/aarch64-linux-gnu/libvpx.so.7 dennis-mb:/opt/box/gen/avlibs/aarch64-linux-gnu
-scp /usr/lib/aarch64-linux-gnu/libdav1d.so.5 dennis-mb:/opt/box/gen/avlibs/aarch64-linux-gnu
-scp /usr/lib/aarch64-linux-gnu/libcodec2.so.1.0 dennis-mb:/opt/box/gen/avlibs/aarch64-linux-gnu
-scp /usr/lib/aarch64-linux-gnu/libx264.so.163 dennis-mb:/opt/box/gen/avlibs/aarch64-linux-gnu
-#scp /usr/lib/aarch64-linux-gnu/libmfx.so.1 dennis-mb:/opt/box/gen/avlibs/aarch64-linux-gnu
+scp /usr/lib/aarch64-linux-gnu/libavcodec.so.58 dennis-mb:/opt/box/gen/avlibs/arm64
+scp /usr/lib/aarch64-linux-gnu/libavutil.so.56 dennis-mb:/opt/box/gen/avlibs/arm64
+scp /usr/lib/aarch64-linux-gnu/libswscale.so.5 dennis-mb:/opt/box/gen/avlibs/arm64
+scp /usr/lib/aarch64-linux-gnu/libswresample.so.3 dennis-mb:/opt/box/gen/avlibs/arm64
+scp /usr/lib/aarch64-linux-gnu/libvpx.so.7 dennis-mb:/opt/box/gen/avlibs/arm64
+scp /usr/lib/aarch64-linux-gnu/libdav1d.so.5 dennis-mb:/opt/box/gen/avlibs/arm64
+scp /usr/lib/aarch64-linux-gnu/libcodec2.so.1.0 dennis-mb:/opt/box/gen/avlibs/arm64
+scp /usr/lib/aarch64-linux-gnu/libx264.so.163 dennis-mb:/opt/box/gen/avlibs/arm64
+#scp /usr/lib/aarch64-linux-gnu/libmfx.so.1 dennis-mb:/opt/box/gen/avlibs/arm64
